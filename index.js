c8bf265464bc02b1498c02884a93d55588d79b45
Yukizumi-S <87900666+Yukizumi-S@users.noreply.github.com>
2021-07-24 21:10:25 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function Check1() {
+    var res=confirm("新しいタブで開きます。よろしいですか？");
+    if(res==true){
+        window.open('https://www.google.co.jp/');
+    }else{
+        alert("開くのを中止しました。");
+    }
+}
+function Check2() {
+    var res=confirm("新しいタブで開きます。よろしいですか？");
+    if(res==true){
+        window.open('https://www.yahoo.co.jp/');
+    }else{
+        alert("開くのを中止しました。");
+    }
+}
+function Check3() {
+    var res=confirm("新しいタブで開きます。よろしいですか？");
+    if(res==true){
+        window.open('https://www.ugtop.com/spill.shtml');
+    }else{
+        alert("開くのを中止しました。");
+    }
+}
+function Check4() {
+    var res=confirm("新しいタブで開きます。よろしいですか？");
+    if(res==true){
+        window.open('https://www.amazon.co.jp/');
+    }else{
+        alert("開くのを中止しました。");
+    }
+}
